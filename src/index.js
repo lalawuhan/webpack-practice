67c4a71ebdf033b3318f9eb9231dc453bd7a3257
Rethabile Segoe <precious.segoe@gmail.com>
2020-08-07 11:33:05 +0200

    add btn logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import moment from "moment";
+//with a dynamic import you can choose when to load code:
+const getUserModule = () => import("./common/usersAPI");
+
+//If you want to control the "chunk name"
+//import(/* webpackChunkName: "usersAPI" */ "./common/usersAPI");
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  //load module dynamically
+  getUserModule().then(({ getUsers }) => {
+    getUsers().then((json) => console.log(json));
+  });
+});
 
 //initial code commented out to try code splitting with moment.js
 // import "./style.css";
